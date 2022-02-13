bd9993c91f1b8df8d98faa881f83452a30fa48f9
Barun Debnath <barundebnath91@gmail.com>
2022-02-13 10:38:31 +0530

    nextjs project started

---
+++ ReactApp/pages/index.js
@@ -0,0 +1,9 @@
+import "flowbite";
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <Navbar />;
+}
