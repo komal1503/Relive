35e8d5fa5ec637779448e3f072ca7a8dab98557f
Barun Debnath <barundebnath91@gmail.com>
2022-02-13 11:22:01 +0530

    memory card and modal done

---
+++ ReactApp/pages/memory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MemoryCard from "../components/MemoryCard";
+
+const MemoryPage = () => {
+  return (
+    <div>
+      <MemoryCard />
+    </div>
+  );
+};
+
+export default MemoryPage;
